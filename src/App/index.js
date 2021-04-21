f663239629e6173ad73a5f979fb0aaa5661967fa
SaraSchoonover <saramschoon@gmail.com>
2021-04-21 17:15:08 -0500

    added set up branch

---
+++ src/App/index.js
@@ -0,0 +1,13 @@
+import './App.scss';
+import React from 'react';
+
+function App() {
+  return (
+    <div className='App'>
+      <h2>INSIDE APP COMPONENT</h2>
+      <button className='btn btn-info'>I am a button</button>
+    </div>
+  );
+}
+
+export default App;
